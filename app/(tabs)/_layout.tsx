6981f31513c20743e990dcf5ad26c6a60a8702da
nilesh-30 <nileshk7017@gmail.com>
2025-12-30 00:24:19 +0530

    created tab navigation

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,27 @@
+import { Tabs } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
+
+export default function TabsLayout() {
+    return (
+        <Tabs>
+            <Tabs.Screen 
+                name="index"
+                options={{
+                    title: "Todos",
+                    tabBarIcon: ({ color, size }) => (
+                        <Ionicons name="flash-outline" size={size} color={color} />
+                    )
+                }} 
+            />
+            <Tabs.Screen 
+                name="settings"
+                options={{
+                    title: "Settings",
+                    tabBarIcon: ({ color, size }) => (
+                        <Ionicons name="settings" size={size} color={color} />
+                    )
+                }}
+            />
+        </Tabs>
+    )
+};

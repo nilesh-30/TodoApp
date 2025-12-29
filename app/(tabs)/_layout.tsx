3b2d263831dfdcf2f36a413aaffd3a3c54f0107b
nilesh-30 <nileshk7017@gmail.com>
2025-12-30 03:19:41 +0530

    build all components and setup convex

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,19 +1,42 @@
 import { Tabs } from "expo-router";
 import { Ionicons } from "@expo/vector-icons";
+import useTheme from "@/hooks/useTheme";
 
 export default function TabsLayout() {
+
+    const { colors } = useTheme();
+
+
     return (
-        <Tabs>
-            <Tabs.Screen 
+        <Tabs
+            screenOptions={{
+                tabBarActiveTintColor: colors.primary,
+                tabBarInactiveTintColor: colors.textMuted,
+                tabBarStyle: {
+                    backgroundColor: colors.surface,
+                    borderTopWidth: 1,
+                    borderTopColor: colors.border,
+                    height: 90,
+                    paddingBottom: 30,
+                    paddingTop: 10,
+                },
+                tabBarLabelStyle: {
+                    fontSize: 12,
+                    fontWeight: "600",
+                },
+                headerShown: false,
+            }}
+        >
+            <Tabs.Screen
                 name="index"
                 options={{
                     title: "Todos",
                     tabBarIcon: ({ color, size }) => (
                         <Ionicons name="flash-outline" size={size} color={color} />
                     )
-                }} 
+                }}
             />
-            <Tabs.Screen 
+            <Tabs.Screen
                 name="settings"
                 options={{
                     title: "Settings",
